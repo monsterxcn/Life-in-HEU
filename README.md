056266b586aab01273f7d052696129256bf04702
monsterxcn <monsterxcn@gmail.com>
2020-06-13 00:42:23 +0800

    add: Deploy to Workers Sites

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ HEUer 专属导航页 - 衍生自南哪指南
 本项目的一些修改：
 
 * [新增] 适用于 Fork 仓库的 GitHub Actions（雾
+* [新增] 适用于 Cloudflare Workers Sites 的部署方式
 * [修改] 规整文件夹路径，现在只有 css js fonts img 了
 * [修改] 为模块添加 crossorigin 属性防止跨源报错
 * [修改] 腾讯内置浏览器提示，css 替换了丑丑的图片和遮罩
@@ -31,6 +32,44 @@ HEUer 专属导航页 - 衍生自南哪指南
  - 投稿、建议或意见？ [Issue](https://github.com/monsterxcn/Life-in-HEU/issues/new) 请
  - 贵校也想整一个？ Fork -> `public/data.json` 请
  - 开发与 Debug？ `yarn install|serve|lint|build` 请
+
+## Cloudflare Workers Sites
+
+参考《[将 Hexo 部署到 Cloudflare Workers Site 上的趟坑记录 | Sukka's Blog](https://blog.skk.moe/post/deploy-blog-to-cf-workers-site/#%E8%87%AA%E5%AE%9A%E4%B9%89-Cloudflare-Workers-Site-%E7%9A%84%E8%A1%8C%E4%B8%BA)》，本仓库尝试将导航页部署至 Cloudflare Workers Sites 以提升浏览体验。
+
+以下部署过程仅供参考。
+
+ - 安装 Wrangler CLI
+
+```bash
+$ npm i @cloudflare/wrangler -g
+# yarn global add @cloudflare/wrangler
+```
+
+ - 创建 Cloudflare API Token
+
+从 [这里](https://dash.cloudflare.com/profile/api-tokens) 申请一个 API Token
+
+ - 创建 GitHub Repo Secrets
+
+在 GitHub 仓库的设置页面添加 Secrets 环境变量，内容为之前生成的 Cloudflare 的 API Token
+
+ - 修改 GitHub Actions
+
+如果没有绑定域名，请使用默认域名，自行修改 wrangler.toml
+
+wrangler.toml 示例：
+
+```
+name = "cfworkers-test"
+type = "webpack"
+workers_dev = true
+account_id = "c061f1cd........."
+
+[site]
+bucket = "./dist"
+entry-point = "workers-site"
+```
 
 ## LICENSE
 
---
+++ workers-site/index.js
@@ -0,0 +1,100 @@
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+
+/**
+ * The DEBUG flag will do two things that help during development:
+ * 1. we will skip caching on the edge, which makes it easier to
+ *    debug.
+ * 2. we will return an error message on exception in your Response rather
+ *    than the default 404.html page.
+ */
+const DEBUG = false
+
+addEventListener('fetch', event => {
+  try {
+    event.respondWith(handleEvent(event))
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        }),
+      )
+    }
+    event.respondWith(new Response('Internal Error', { status: 500 }))
+  }
+})
+
+async function handleEvent(event) {
+  const url = new URL(event.request.url);
+  const { origin, pathname: path, search } = new URL(event.request.url);
+  let options = {}
+
+  /**
+   * You can add custom logic to how we fetch your assets
+   * by configuring the function `mapRequestToAsset`
+   */
+  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+
+  try {
+    if (DEBUG) {
+      // customize caching
+      options.cacheControl = {
+        bypassCache: true,
+      }
+    }
+    if (path.endsWith('/index.html')) {
+      return new Response(null, {
+        status: 301,
+          headers: {
+            'Location': `${origin}${path.substring(0, path.length - 10)}${search}`,
+            'Cache-Control': 'max-age=3600'
+          }
+      });
+    }
+    if (path.endsWith('.svg') || path.endsWith('.eot') || path.endsWith('.ttf') || path.endsWith('.woff') || path.endsWith('.woff2') || path.endsWith('.css') || path.endsWith('.js')) {
+      const response = await getAssetFromKV(event, {
+        cacheControl: {
+          edgeTtl: 365 * 24 * 60 * 60,
+          browserTtl: 365 * 24 * 60 * 60,
+        }
+      });
+      response.headers.set('cache-control', `public, max-age=${365 * 24 * 60 * 60}, immutable`);
+      return response;
+    }
+    return await getAssetFromKV(event, options)
+  } catch (e) {
+    // if an error is thrown try to serve the asset at 404.html
+    if (!DEBUG) {
+      try {
+        let notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
+        })
+
+        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+      } catch (e) {}
+    }
+
+    return new Response(e.message || e.toString(), { status: 500 })
+  }
+}
+
+/**
+ * Here's one example of how to modify a request to
+ * remove a specific prefix, in this case `/docs` from
+ * the url. This can be useful if you are deploying to a
+ * route on a zone, or if you only want your static content
+ * to exist at a specific path.
+ */
+function handlePrefix(prefix) {
+  return request => {
+    // compute the default (e.g. / -> index.html)
+    let defaultAssetKey = mapRequestToAsset(request)
+    let url = new URL(defaultAssetKey.url)
+
+    // strip the prefix from the path for lookup
+    url.pathname = url.pathname.replace(prefix, '/')
+
+    // inherit all other props from the default request
+    return new Request(url.toString(), defaultAssetKey)
+  }
+}
